970b90bbb770c10b56c2aff9f87bda06b2b6bef2
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-02-24 04:42:10 +0900

    chore(core): remove unnecessary files

--- packages/core/src/types/index.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentProps, JSXElementConstructor } from 'react'
-
-export type ComponentPropsWithoutChildren<Component extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>> =
-  Omit<ComponentProps<Component>, 'children'>
