1156066f0cc58f886b47f7a255ec05b98ebd9df0
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-02-24 17:17:14 +0900

    chore: remove unnecessary files

--- packages/styled-components/.eslintrc.js
+++ packages/styled-components/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ...require('@jsxcss/eslint/styled-components.js'),
+  ...require('@jsxcss/eslint/react.js'),
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json',
