af904c79c49332d505be7e1d5a2269eadf7ae9cd
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-05-08 22:46:00 +0900

    refactor(websites/emotion): _app.tsx simply

--- websites/emotion/pages/_app.tsx
+++ websites/emotion/pages/_app.tsx
@@ -1,22 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { MediaQueryProvider, Stack } from '@jsxcss/emotion'
-import Link from 'next/link'
-import { WindowSizeSensor } from '../components'
+import { MediaQueryProvider } from '@jsxcss/emotion'
+import { Navigation, WindowSizeSensor } from '../components'
 
 const mediaQueryPxs = [0, 768, 1440]
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <MediaQueryProvider pxs={mediaQueryPxs}>
+    <Navigation />
+    <Component pageProps={pageProps} />
     <WindowSizeSensor mediaQueryPxs={mediaQueryPxs} />
-    <Stack.Horizontal paddingTop={[80, 0, 0]} paddingLeft={[12]} align="center" fontSize={[16, 18, 20]}>
-      <Link href="/">
-        <h1>@jsxcss/emotion</h1>
-      </Link>
-    </Stack.Horizontal>
-    <Stack.Vertical>
-      <Component pageProps={pageProps} />
-    </Stack.Vertical>
   </MediaQueryProvider>
 )
 
